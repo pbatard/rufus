55e6cd80236b4cb67dcaac3d644c02fe82d58dcc
Pete Batard <pete@akeo.ie>
2012-01-06 16:46:27 +0000

    [mingw] added --with-freedos to configure
    * default is enabled (=> add --without-freedos to disable)
    * also update the release process as a result
    * also update version to rufus next
    * also fixed FreeDOS embedded files lost when RC is edited in Visual Studio

---
+++ _release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rm -f rufus*.exe
+./configure --without-freedos
+make clean
+make release -j2
+./configure --with-freedos
+# The only difference between FreeDOS and non FreeDOS is with the RC
+# => instead of invoking 'make clean, just remove the RC object
+rm src/rufus_rc.o
+make release -j2
--- _sign.cmd
+++ _sign.cmd
@@ -1,5 +1,4 @@
 :retry
-@set password=
 @set /p password=Please enter PFX password: 
 @E:\WinDDK\7600.16385.0\bin\amd64\signtool sign /v /f D:\Secured\akeo\pbatard.p12 /p %password% /t http://time.certum.pl %1 %2 %3 %4
 @if ERRORLEVEL 1 goto retry
