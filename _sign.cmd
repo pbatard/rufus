ad0226a39417dbce5e9efc479f8665d92eea5990
Pete Batard <pete@akeo.ie>
2012-11-13 01:33:38 +0000

    [misc] set rufus-next to 1.2.1
    * Also update signing script to reflect new Akeo certificate
      NB: If you value your time, do *NOT* purchase certs from Comodo!!!

--- _sign.cmd
+++ _sign.cmd
@@ -1,6 +1,6 @@
 :retry
 @set /p password=Please enter PFX password: 
-@E:\WinDDK\7600.16385.0\bin\amd64\signtool sign /v /f D:\Secured\akeo\pbatard.p12 /p %password% /t http://time.certum.pl %1 %2 %3 %4
+@E:\WinDDK\7600.16385.0\bin\amd64\signtool sign /v /f D:\Secured\akeo\akeo.p12 /p %password% /tr http://timestamp.comodoca.com/rfc3161 %1 %2 %3 %4
 @if ERRORLEVEL 1 goto retry
 @set password=
 @exit
