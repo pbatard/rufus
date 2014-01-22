pbatard/rufus

commit 299506056ac441f23d25839525741832d26865e9

[syslinux] fix support for recent Syslinux versions
* Detect Syslinux version from isolinux.bin (Closes #272)
* Download required Syslinux v5+ files where needed (Closes #165)
* Also fix an issue where using a Syslinux based ISO didn't switch to ISO mode
* Also alter DownloadFile and ExtractISOFile to return a size

src/iso.c
@@ -55,14 +55,16 @@ void cdio_destroy (CdIo_t* p_cdio) {}
 
 RUFUS_ISO_REPORT iso_report;
 int64_t iso_blocking_status = -1;
-BOOL enable_joliet = TRUE, enable_rockridge = TRUE;
+BOOL enable_joliet = TRUE, enable_rockridge = TRUE, has_ldlinux_c32;
 #define ISO_BLOCKING(x) do {x; iso_blocking_status++; } while(0)
 static const char* psz_extract_dir;
 static const char* bootmgr_efi_name = "bootmgr.efi";
 static const char* ldlinux_name = "ldlinux.sys";
-static const char* syslinux_v5_file = "ldlinux.c32";
+static const char* ldlinux_c32 = "ldlinux.c32";
 static const char* efi_dirname = "/efi/boot";
-static const char* isolinux_name[] = { "isolinux.cfg", "syslinux.cfg", "extlinux.conf"};
+static const char* syslinux_cfg[] = { "isolinux.cfg", "syslinux.cfg", "extlinux.conf"};
+static const char dot_isolinux_bin[] = ".\\isolinux.bin";
+static const char* isolinux_bin = &dot_isolinux_bin[2];
 static const char* pe_dirname[] = { "/i386", "/minint" };
 static const char* pe_file[] = { "ntdetect.com", "setupldr.bin", "txtsetup.sif" };
 static const char* reactos_name = "setupldr.sys"; // TODO: freeldr.sys doesn't seem to work
@@ -72,7 +74,7 @@ static const int64_t old_c32_threshold[NB_OLD_C32] = OLD_C32_THRESHOLD;
 static uint8_t i_joliet_level = 0;
 static uint64_t total_blocks, nb_blocks;
 static BOOL scan_only = FALSE;
-static StrArray config_path;
+static StrArray config_path, isolinux_path;
 
 // TODO: Timestamp & permissions preservation
 
@@ -117,6 +119,7 @@ static void log_handler (cdio_log_level_t level, const char *message)
 {
 	switch(level) {
 	case CDIO_LOG_DEBUG:
+		// TODO: use a registry key to enable libcdio debug?
 		return;
 	default:
 		uprintf("libcdio: %s\n", message);
@@ -134,14 +137,14 @@ static BOOL check_iso_props(const char* psz_dirname, BOOL* is_syslinux_cfg, BOOL
 
 	// Check for an isolinux/syslinux config file anywhere
 	*is_syslinux_cfg = FALSE;
-	for (i=0; i<ARRAYSIZE(isolinux_name); i++) {
-		if (safe_stricmp(psz_basename, isolinux_name[i]) == 0)
+	for (i=0; i<ARRAYSIZE(syslinux_cfg); i++) {
+		if (safe_stricmp(psz_basename, syslinux_cfg[i]) == 0)
 			*is_syslinux_cfg = TRUE;
 	}
 
-	// Check for a syslinux v5.0 file anywhere
-	if (safe_stricmp(psz_basename, syslinux_v5_file) == 0) {
-		iso_report.has_syslinux_v5 = TRUE;
+	// Check for a syslinux v5.0+ file anywhere
+	if (safe_stricmp(psz_basename, ldlinux_c32) == 0) {
+		has_ldlinux_c32 = TRUE;
 	}
 
 	// Check for an old incompatible c32 file anywhere
@@ -178,10 +181,14 @@ static BOOL check_iso_props(const char* psz_dirname, BOOL* is_syslinux_cfg, BOOL
 						iso_report.winpe |= (1<<i)<<(ARRAYSIZE(pe_dirname)*j);
 
 		if (*is_syslinux_cfg) {
-			iso_report.has_isolinux = TRUE;
 			// Maintain a list of all the isolinux/syslinux configs identified so far
 			StrArrayAdd(&config_path, psz_fullpath);
 		}
+		if (safe_stricmp(psz_basename, isolinux_bin) == 0) {
+			// Maintain a list of all the isolinux.bin files found
+			StrArrayAdd(&isolinux_path, psz_fullpath);
+		}
+
 		for (i=0; i<NB_OLD_C32; i++) {
 			if (is_old_c32[i])
 				iso_report.has_old_c32[i] = TRUE;
@@ -465,18 +472,20 @@ out:
 
 BOOL ExtractISO(const char* src_iso, const char* dest_dir, BOOL scan)
 {
-	size_t i;
+	size_t i, k, size;
 	int j;
+	uint16_t sl_version;
 	FILE* fd;
 	BOOL r = FALSE;
 	iso9660_t* p_iso = NULL;
 	udf_t* p_udf = NULL; 
 	udf_dirent_t* p_udf_root;
 	LONG progress_style;
-	char* tmp;
-	char path[64];
+	char *tmp, *buf;
+	char path[MAX_PATH];
 	const char* basedir[] = { "i386", "minint" };
 	const char* tmp_sif = ".\\txtsetup.sif~";
+	const char ISOLINUX[] = { 'I', 'S', 'O', 'L', 'I', 'N', 'U', 'X', ' ' };
 	iso_extension_mask_t iso_extension_mask = ISO_EXTENSION_ALL;
 
 	if ((src_iso == NULL) || (dest_dir == NULL))
@@ -489,8 +498,10 @@ BOOL ExtractISO(const char* src_iso, const char* dest_dir, BOOL scan)
 	if (scan_only) {
 		total_blocks = 0;
 		memset(&iso_report, 0, sizeof(iso_report));
+		has_ldlinux_c32 = FALSE;
 		// String array of all isolinux/syslinux locations
 		StrArrayCreate(&config_path, 8);
+		StrArrayCreate(&isolinux_path, 8);
 		// Change the Window title and static text
 		SetWindowTextU(hISOProgressDlg, lmprintf(MSG_202));
 		SetWindowTextU(hISOFileName, lmprintf(MSG_202));
@@ -566,14 +577,65 @@ out:
 		// We use the fact that UDF_BLOCKSIZE and ISO_BLOCKSIZE are the same here
 		iso_report.projected_size = total_blocks * ISO_BLOCKSIZE;
 		// We will link the existing isolinux.cfg from a syslinux.cfg we create
-		// If multiple config file exist, choose the one with the shortest path
-		if (iso_report.has_isolinux) {
+		// If multiple config files exist, choose the one with the shortest path
+		// (so that a '/syslinux.cfg' is preferred over a '/isolinux/isolinux.cfg')
+		if (!IsStrArrayEmpty(config_path)) {
 			safe_strcpy(iso_report.cfg_path, sizeof(iso_report.cfg_path), config_path.Table[0]);
 			for (i=1; i<config_path.Index; i++) {
 				if (safe_strlen(iso_report.cfg_path) > safe_strlen(config_path.Table[i]))
 					safe_strcpy(iso_report.cfg_path, sizeof(iso_report.cfg_path), config_path.Table[i]);
 			}
 			uprintf("Will use %s for Syslinux\n", iso_report.cfg_path);
+			// Extract all of the isolinux.bin files we found to identify their versions
+			for (i=0; i<isolinux_path.Index; i++) {
+				size = (size_t)ExtractISOFile(src_iso, isolinux_path.Table[i], dot_isolinux_bin);
+				if (size == 0) {
+					uprintf("Could not access %s\n", isolinux_path.Table[i]);
+				} else {
+					buf = (char*)calloc(size, 1);
+					if (buf == NULL) break;
+					fd = fopen(dot_isolinux_bin, "rb");
+					if (fd == NULL) {
+						free(buf);
+						continue;
+					}
+					fread(buf, 1, size, fd);
+					fclose(fd);
+					for (k=0; k<size-16; k++) {
+						if (memcmp(&buf[k], ISOLINUX, sizeof(ISOLINUX)) == 0) {
+							k += sizeof(ISOLINUX);
+							sl_version = (((uint8_t)strtoul(&buf[k], &tmp, 10))<<8) + (uint8_t)strtoul(&tmp[1], NULL, 10);
+							if (iso_report.sl_version == 0) {
+								iso_report.sl_version = sl_version;
+								j = (int)i;
+							} else if (iso_report.sl_version != sl_version) {
+								uprintf("Found conflicting %s versions:\n  '%s' (v%d.%02d) vs '%s' (v%d.%02d)\n", isolinux_bin,
+									isolinux_path.Table[j], SL_MAJOR(iso_report.sl_version), SL_MINOR(iso_report.sl_version),
+									isolinux_path.Table[i], SL_MAJOR(sl_version), SL_MINOR(sl_version));
+							}
+							break;
+						}
+					}
+					free(buf);
+					_unlink(dot_isolinux_bin);
+				}
+			}
+			if (iso_report.sl_version != 0) {
+				static_sprintf(iso_report.sl_version_str, "v%d.%02d",
+					SL_MAJOR(iso_report.sl_version), SL_MINOR(iso_report.sl_version));
+				uprintf("Detected Isolinux version: %s (from '%s')",
+					iso_report.sl_version_str, isolinux_path.Table[j]);
+				if ( (has_ldlinux_c32 && (SL_MAJOR(iso_report.sl_version) < 5))
+				  || (!has_ldlinux_c32 && (SL_MAJOR(iso_report.sl_version) >= 5)) )
+					uprintf("Warning: Conflict between Isolinux version and the presence of ldlinux.c32...\n");
+			} else {
+				// Couldn't find a version from isolinux.bin. Force set to the versions we embed
+				iso_report.sl_version = embedded_sl_version[has_ldlinux_c32?1:0];
+				static_sprintf(iso_report.sl_version_str, "v%d.%02d",
+					SL_MAJOR(iso_report.sl_version), SL_MINOR(iso_report.sl_version));
+				uprintf("Warning: Could not detect Isolinux version - Forcing to %s (embedded)",
+					iso_report.sl_version_str);
+			}
 		}
 		if (IS_WINPE(iso_report.winpe)) {
 			// In case we have a WinPE 1.x based iso, we extract and parse txtsetup.sif
@@ -593,7 +655,8 @@ out:
 			safe_free(tmp);
 		}
 		StrArrayDestroy(&config_path);
-	} else if (iso_report.has_isolinux) {
+		StrArrayDestroy(&isolinux_path);
+	} else if (!IsStrArrayEmpty(config_path)) {
 		safe_sprintf(path, sizeof(path), "%s\\syslinux.cfg", dest_dir);
 		// Create a /syslinux.cfg (if none exists) that points to the existing isolinux cfg
 		fd = fopen(path, "r");
@@ -626,14 +689,14 @@ out:
 	return (r == 0);
 }
 
-BOOL ExtractISOFile(const char* iso, const char* iso_file, const char* dest_file)
+int64_t ExtractISOFile(const char* iso, const char* iso_file, const char* dest_file)
 {
 	size_t i;
 	ssize_t read_size;
 	int64_t file_length;
 	char buf[UDF_BLOCKSIZE];
 	DWORD buf_size, wr_size;
-	BOOL s, r = FALSE;
+	BOOL s, r = 0;
 	iso9660_t* p_iso = NULL;
 	udf_t* p_udf = NULL; 
 	udf_dirent_t *p_udf_root = NULL, *p_udf_file = NULL;
@@ -678,8 +741,8 @@ BOOL ExtractISOFile(const char* iso, const char* iso_file, const char* dest_file
 			goto out;
 		}
 		file_length -= read_size;
+		r += read_size;
 	}
-	r = TRUE;
 	goto out;
 
 try_iso:
@@ -710,8 +773,8 @@ try_iso:
 			goto out;
 		}
 		file_length -= ISO_BLOCKSIZE;
+		r += ISO_BLOCKSIZE;
 	}
-	r = TRUE;
 
 out:
 	safe_closehandle(file_handle);
@@ -726,5 +789,5 @@ out:
 		iso9660_close(p_iso);
 	if (p_udf != NULL)
 		udf_close(p_udf);
-	return (r == 0);
+	return r;
 }

commit 3c2873bbdcae1bdee090f3eec659d51521ac4bf9

[syslinux] fix missing syslinux.cfg creation
* Issue was introduced with 299506056ac441f23d25839525741832d26865e9
* Also improve StrArray definition

src/iso.c
@@ -580,17 +580,17 @@ out:
 		// If multiple config files exist, choose the one with the shortest path
 		// (so that a '/syslinux.cfg' is preferred over a '/isolinux/isolinux.cfg')
 		if (!IsStrArrayEmpty(config_path)) {
-			safe_strcpy(iso_report.cfg_path, sizeof(iso_report.cfg_path), config_path.Table[0]);
+			safe_strcpy(iso_report.cfg_path, sizeof(iso_report.cfg_path), config_path.String[0]);
 			for (i=1; i<config_path.Index; i++) {
-				if (safe_strlen(iso_report.cfg_path) > safe_strlen(config_path.Table[i]))
-					safe_strcpy(iso_report.cfg_path, sizeof(iso_report.cfg_path), config_path.Table[i]);
+				if (safe_strlen(iso_report.cfg_path) > safe_strlen(config_path.String[i]))
+					safe_strcpy(iso_report.cfg_path, sizeof(iso_report.cfg_path), config_path.String[i]);
 			}
 			uprintf("Will use %s for Syslinux\n", iso_report.cfg_path);
 			// Extract all of the isolinux.bin files we found to identify their versions
 			for (i=0; i<isolinux_path.Index; i++) {
-				size = (size_t)ExtractISOFile(src_iso, isolinux_path.Table[i], dot_isolinux_bin);
+				size = (size_t)ExtractISOFile(src_iso, isolinux_path.String[i], dot_isolinux_bin);
 				if (size == 0) {
-					uprintf("Could not access %s\n", isolinux_path.Table[i]);
+					uprintf("Could not access %s\n", isolinux_path.String[i]);
 				} else {
 					buf = (char*)calloc(size, 1);
 					if (buf == NULL) break;
@@ -610,8 +610,8 @@ out:
 								j = (int)i;
 							} else if (iso_report.sl_version != sl_version) {
 								uprintf("Found conflicting %s versions:\n  '%s' (v%d.%02d) vs '%s' (v%d.%02d)\n", isolinux_bin,
-									isolinux_path.Table[j], SL_MAJOR(iso_report.sl_version), SL_MINOR(iso_report.sl_version),
-									isolinux_path.Table[i], SL_MAJOR(sl_version), SL_MINOR(sl_version));
+									isolinux_path.String[j], SL_MAJOR(iso_report.sl_version), SL_MINOR(iso_report.sl_version),
+									isolinux_path.String[i], SL_MAJOR(sl_version), SL_MINOR(sl_version));
 							}
 							break;
 						}
@@ -624,7 +624,7 @@ out:
 				static_sprintf(iso_report.sl_version_str, "v%d.%02d",
 					SL_MAJOR(iso_report.sl_version), SL_MINOR(iso_report.sl_version));
 				uprintf("Detected Isolinux version: %s (from '%s')",
-					iso_report.sl_version_str, isolinux_path.Table[j]);
+					iso_report.sl_version_str, isolinux_path.String[j]);
 				if ( (has_ldlinux_c32 && (SL_MAJOR(iso_report.sl_version) < 5))
 				  || (!has_ldlinux_c32 && (SL_MAJOR(iso_report.sl_version) >= 5)) )
 					uprintf("Warning: Conflict between Isolinux version and the presence of ldlinux.c32...\n");
@@ -656,7 +656,7 @@ out:
 		}
 		StrArrayDestroy(&config_path);
 		StrArrayDestroy(&isolinux_path);
-	} else if (!IsStrArrayEmpty(config_path)) {
+	} else if (HAS_SYSLINUX(iso_report)) {
 		safe_sprintf(path, sizeof(path), "%s\\syslinux.cfg", dest_dir);
 		// Create a /syslinux.cfg (if none exists) that points to the existing isolinux cfg
 		fd = fopen(path, "r");

src/rufus.c
@@ -589,7 +589,7 @@ static BOOL PopulateProperties(int ComboIndex)
 	SetTargetSystem();
 	SetFSFromISO();
 	EnableBootOptions(TRUE);
-	device_tooltip = (char*) malloc(safe_strlen(DriveID.Table[ComboIndex]) + 16);
+	device_tooltip = (char*) malloc(safe_strlen(DriveID.String[ComboIndex]) + 16);
 
 	// Set a proposed label according to the size (eg: "256MB", "8GB")
 	if (HumanReadableSize < 1.0) {
@@ -601,14 +601,14 @@ static BOOL PopulateProperties(int ComboIndex)
 		safe_sprintf(SelectedDrive.proposed_label, sizeof(SelectedDrive.proposed_label),
 			"%0.0f%s", ceil(HumanReadableSize), lmprintf(MSG_020+i));
 		if (device_tooltip != NULL)
-			safe_sprintf(device_tooltip, safe_strlen(DriveID.Table[ComboIndex]) + 16,
-				"%s (%0.0f%s)", DriveID.Table[ComboIndex], ceil(HumanReadableSize), lmprintf(MSG_020+i));
+			safe_sprintf(device_tooltip, safe_strlen(DriveID.String[ComboIndex]) + 16,
+				"%s (%0.0f%s)", DriveID.String[ComboIndex], ceil(HumanReadableSize), lmprintf(MSG_020+i));
 	} else {
 		safe_sprintf(SelectedDrive.proposed_label, sizeof(SelectedDrive.proposed_label),
 			"%0.2f%s", HumanReadableSize, lmprintf(MSG_020+i));
 		if (device_tooltip != NULL)
-			safe_sprintf(device_tooltip, safe_strlen(DriveID.Table[ComboIndex]) + 16,
-				"%s (%0.2f%s)", DriveID.Table[ComboIndex], HumanReadableSize, lmprintf(MSG_020+i));
+			safe_sprintf(device_tooltip, safe_strlen(DriveID.String[ComboIndex]) + 16,
+				"%s (%0.2f%s)", DriveID.String[ComboIndex], HumanReadableSize, lmprintf(MSG_020+i));
 	}
 
 	// Add a tooltip (with the size of the device in parenthesis)
@@ -618,11 +618,11 @@ static BOOL PopulateProperties(int ComboIndex)
 
 	// If no existing label is available and no ISO is selected, propose one according to the size (eg: "256MB", "8GB")
 	if ((iso_path == NULL) || (iso_report.label[0] == 0)) {
-		if ( (safe_stricmp(no_label, DriveLabel.Table[ComboIndex]) == 0)
-		  || (safe_stricmp(lmprintf(MSG_207), DriveLabel.Table[ComboIndex]) == 0) ) {
+		if ( (safe_stricmp(no_label, DriveLabel.String[ComboIndex]) == 0)
+		  || (safe_stricmp(lmprintf(MSG_207), DriveLabel.String[ComboIndex]) == 0) ) {
 			SetWindowTextU(hLabel, SelectedDrive.proposed_label);
 		} else {
-			SetWindowTextU(hLabel, DriveLabel.Table[ComboIndex]);
+			SetWindowTextU(hLabel, DriveLabel.String[ComboIndex]);
 		}
 	} else {
 		SetWindowTextU(hLabel, iso_report.label);

src/rufus.h
@@ -369,8 +369,7 @@ static __inline void *_reallocf(void *ptr, size_t size)
 
 /* Basic String Array */
 typedef struct {
-	// TODO: rename 'Table' to 'String'
-	char** Table;
+	char** String;
 	size_t Index;	// Current array size
 	size_t Max;		// Maximum array size
 } StrArray;

src/rufus.rc
@@ -33,7 +33,7 @@ LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
 IDD_DIALOG DIALOGEX 12, 12, 206, 329
 STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | DS_CENTER | WS_POPUP | WS_CAPTION | WS_SYSMENU
 EXSTYLE WS_EX_APPWINDOW
-CAPTION "Rufus v1.4.3.387"
+CAPTION "Rufus v1.4.3.388"
 FONT 8, "MS Shell Dlg", 400, 0, 0x1
 BEGIN
     DEFPUSHBUTTON   "Start",IDC_START,94,291,50,14
@@ -288,8 +288,8 @@ END
 //
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 1,4,3,387
- PRODUCTVERSION 1,4,3,387
+ FILEVERSION 1,4,3,388
+ PRODUCTVERSION 1,4,3,388
  FILEFLAGSMASK 0x3fL
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -306,13 +306,13 @@ BEGIN
         BEGIN
             VALUE "CompanyName", "Akeo Consulting (http://akeo.ie)"
             VALUE "FileDescription", "Rufus"
-            VALUE "FileVersion", "1.4.3.387"
+            VALUE "FileVersion", "1.4.3.388"
             VALUE "InternalName", "Rufus"
             VALUE "LegalCopyright", "© 2011-2014 Pete Batard (GPL v3)"
             VALUE "LegalTrademarks", "http://www.gnu.org/copyleft/gpl.html"
             VALUE "OriginalFilename", "rufus.exe"
             VALUE "ProductName", "Rufus"
-            VALUE "ProductVersion", "1.4.3.387"
+            VALUE "ProductVersion", "1.4.3.388"
         END
     END
     BLOCK "VarFileInfo"

src/stdfn.c
@@ -1,7 +1,7 @@
 /*
  * Rufus: The Reliable USB Formatting Utility
  * Standard Windows function calls
- * Copyright © 2013 Pete Batard <pete@akeo.ie>
+ * Copyright © 2013-2014 Pete Batard <pete@akeo.ie>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -151,28 +151,28 @@ void StrArrayCreate(StrArray* arr, size_t initial_size)
 {
 	if (arr == NULL) return;
 	arr->Max = initial_size; arr->Index = 0;
-	arr->Table = (char**)calloc(arr->Max, sizeof(char*));
-	if (arr->Table == NULL)
+	arr->String = (char**)calloc(arr->Max, sizeof(char*));
+	if (arr->String == NULL)
 		uprintf("Could not allocate string array\n");
 }
 
 void StrArrayAdd(StrArray* arr, const char* str)
 {
 	char** old_table;
-	if ((arr == NULL) || (arr->Table == NULL))
+	if ((arr == NULL) || (arr->String == NULL))
 		return;
 	if (arr->Index == arr->Max) {
 		arr->Max *= 2;
-		old_table = arr->Table;
-		arr->Table = (char**)realloc(arr->Table, arr->Max*sizeof(char*));
-		if (arr->Table == NULL) {
+		old_table = arr->String;
+		arr->String = (char**)realloc(arr->String, arr->Max*sizeof(char*));
+		if (arr->String == NULL) {
 			free(old_table);
 			uprintf("Could not reallocate string array\n");
 			return;
 		}
 	}
-	arr->Table[arr->Index] = safe_strdup(str);
-	if (arr->Table[arr->Index++] == NULL) {
+	arr->String[arr->Index] = safe_strdup(str);
+	if (arr->String[arr->Index++] == NULL) {
 		uprintf("Could not store string in array\n");
 	}
 }
@@ -180,10 +180,10 @@ void StrArrayAdd(StrArray* arr, const char* str)
 void StrArrayClear(StrArray* arr)
 {
 	size_t i;
-	if ((arr == NULL) || (arr->Table == NULL))
+	if ((arr == NULL) || (arr->String == NULL))
 		return;
 	for (i=0; i<arr->Index; i++) {
-		safe_free(arr->Table[i]);
+		safe_free(arr->String[i]);
 	}
 	arr->Index = 0;
 }
@@ -192,7 +192,7 @@ void StrArrayDestroy(StrArray* arr)
 {
 	StrArrayClear(arr);
 	if (arr != NULL)
-		safe_free(arr->Table);
+		safe_free(arr->String);
 }
 
 /*