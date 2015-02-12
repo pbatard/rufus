ee1b38e428e05cfa6a2c2d449135a2d0ab940a52
Viszokai György <familyguyfan79@gmail.com>
2015-02-12 19:22:51 +0000

    [loc] update Hungarian translation
    * Also remove Hungarian from M.I.A. list

--- src/rufus.h
+++ src/rufus.h
@@ -34,7 +34,7 @@
 /* Features not ready for prime time and that may *DESTROY* your data - USE AT YOUR OWN RISKS! */
 // #define RUFUS_TEST
 /* Languages for which translators are M.I.A. and that we could use help with */
-#define LOST_TRANSLATORS            { "ar-SA", "da-DK", "hu-HU", "id-ID", "pt-PT" }	// NB: locales MUST be <= 5 chars
+#define LOST_TRANSLATORS            { "ar-SA", "da-DK", "id-ID", "pt-PT" }	// NB: locales MUST be <= 5 chars
 /* Probability of getting the M.I.A. translator message. For more on this, see LostTranslatorCheck() */
 #define LOST_TRANSLATOR_PROBABILITY 1000
 
