9814a638a3e4da79726344f92cf44f603119fb3e
Pete Batard <pete@akeo.ie>
2016-02-03 23:29:34 +0000

    [pki] add both SHA-1 and SHA-256 digital signatures
    * "[Grumble] Stupid Microsoft... [Grumble] Couldn't push updated PKI algorithms
       to Windows platforms... [Grumble] Even if their lives depended on it..."

--- _sign.cmd
+++ _sign.cmd
@@ -1,6 +1,7 @@
 :retry
-@set /p password=Please enter PFX password:
-@"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool" sign /v /fd SHA256 /f D:\Secured\akeo\akeo.p12 /p %password% /tr http://timestamp.comodoca.com/rfc3161 /td SHA256 %1 %2 %3 %4
+@set /p password=Please enter PFX password: 
+@"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool" sign /v /fd SHA1 /f D:\Secured\akeo\sha1\akeo.p12 /p %password% /tr http://timestamp.comodoca.com/rfc3161 /td SHA1 %1 %2 %3 %4
+@"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool" sign /as /v /fd SHA256 /f D:\Secured\akeo\akeo.p12 /p %password% /tr http://timestamp.comodoca.com/rfc3161 /td SHA256 %1 %2 %3 %4
 @if ERRORLEVEL 1 goto retry
 @set password=
 @exit
