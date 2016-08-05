1e9aa1e6465a8df3bfc520ed25754649bcad005a
Pete Batard <pete@akeo.ie>
2016-08-05 21:11:32 +0200

    [appveyor] try without an explicit SDK setup call for x86_64

--- _appveyor_msvc.cmd
+++ _appveyor_msvc.cmd
@@ -1,5 +1,4 @@
 echo on
 SetLocal EnableDelayedExpansion
 
-if [%platform%] == [x86_64] call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /%configuration% /x64
 msbuild rufus.sln /m /p:Configuration=%configuration%,Platform=%platform% /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
