03e01f2a196634097591e87cfa5c1bf4dff6ffd7
Adrian Perez de Castro <aperez@igalia.com>
2016-11-11 11:58:05 +0000

    [misc] Linux cross compilation fixes
    * Closes #850

--- src/format.h
+++ src/format.h
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <Windows.h>
+#include <windows.h>
 #include <winioctl.h>				// for MEDIA_TYPE
 
 #pragma once
