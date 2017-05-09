a4b19ddf1a67ff536718a7253d425107f8df5d31
Pete Batard <pete@akeo.ie>
2017-05-09 10:54:13 +0100

    [misc] move appx generation to Makefile

--- _release.sh
+++ _release.sh
@@ -3,12 +3,3 @@ rm -f rufus*.exe
 ./configure --disable-debug "$@"
 make -j12 clean
 make -j12 release
-
-# Update AppxManifest.xml with the build number
-TAGVER=`git log --oneline | wc -l`
-cat > cmd.sed <<\_EOF
-s/^\([ \t]*\)Version="\([0-9]*\)\.\([0-9]*\)\.[0-9]*\.\([0-9]*\)"\(.*\)/\1Version="\2.\3.@@TAGVER@@.\4"\5/
-_EOF
-sed -i -e "s/@@TAGVER@@/$TAGVER/g" cmd.sed
-sed -b -i -f cmd.sed res/appstore/AppxManifest.xml
-rm cmd.sed
