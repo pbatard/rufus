7c5238f23bc43346568ea24087c78947a7f98bdb
Pete Batard <pete@akeo.ie>
2017-11-17 18:28:44 +0100

    [misc] fix SDK path

--- _sign.cmd
+++ _sign.cmd
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /v /sha1 5759b23dc8f45e9120a7317f306e5b6890b612f0 /fd SHA256 /tr http://timestamp.comodoca.com/rfc3161 /td SHA256 %1
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x64\signtool" sign /v /sha1 5759b23dc8f45e9120a7317f306e5b6890b612f0 /fd SHA256 /tr http://timestamp.comodoca.com/rfc3161 /td SHA256 %1
 exit
