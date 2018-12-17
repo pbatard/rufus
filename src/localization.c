5258bef37fbfedbf79d7e549476acdf4321cb328
kemalinanc13 <45773590+kemalinanc13@users.noreply.github.com>
2018-12-17 23:53:09 +0300

    [misc] changed the copyright year in the comment

--- src/localization.c
+++ src/localization.c
@@ -1,7 +1,7 @@
 /*
  * Rufus: The Reliable USB Formatting Utility
  * Localization functions, a.k.a. "Everybody is doing it wrong but me!"
- * Copyright © 2013-2017 Pete Batard <pete@akeo.ie>
+ * Copyright © 2013-2018 Pete Batard <pete@akeo.ie>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
