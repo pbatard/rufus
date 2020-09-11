pbatard/rufus

commit 77d319267f869d0d9eb9ac5a831164add8a5aa8d

[iso] add Rock Ridge deep directory support
* Closes #1526

src/iso.c
@@ -654,6 +654,10 @@ static int iso_extract_files(iso9660_t* p_iso, const char *psz_path)
 	_CDIO_LIST_FOREACH(p_entnode, p_entlist) {
 		if (FormatStatus) goto out;
 		p_statbuf = (iso9660_stat_t*) _cdio_list_node_data(p_entnode);
+		if ((p_statbuf->rr.b3_rock == yep) && enable_rockridge) {
+			if (p_statbuf->rr.u_su_fields & ISO_ROCK_SUF_PL)
+				img_report.has_deep_directories = TRUE;
+		}
 		// Eliminate . and .. entries
 		if ( (strcmp(p_statbuf->filename, ".") == 0)
 			|| (strcmp(p_statbuf->filename, "..") == 0) )

src/rufus.h
@@ -338,6 +338,7 @@ typedef struct {
 	uint8_t has_symlinks;
 	BOOLEAN has_4GB_file;
 	BOOLEAN has_long_filename;
+	BOOLEAN has_deep_directories;
 	BOOLEAN has_bootmgr;
 	BOOLEAN has_bootmgr_efi;
 	BOOLEAN has_autorun;

src/rufus.rc
@@ -33,7 +33,7 @@ LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
 IDD_DIALOG DIALOGEX 12, 12, 232, 326
 STYLE DS_SETFONT | DS_MODALFRAME | DS_CENTER | WS_MINIMIZEBOX | WS_POPUP | WS_CAPTION | WS_SYSMENU
 EXSTYLE WS_EX_ACCEPTFILES
-CAPTION "Rufus 3.11.1658"
+CAPTION "Rufus 3.11.1659"
 FONT 9, "Segoe UI Symbol", 400, 0, 0x0
 BEGIN
     LTEXT           "Drive Properties",IDS_DRIVE_PROPERTIES_TXT,8,6,53,12,NOT WS_GROUP
@@ -395,8 +395,8 @@ END
 //
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 3,11,1658,0
- PRODUCTVERSION 3,11,1658,0
+ FILEVERSION 3,11,1659,0
+ PRODUCTVERSION 3,11,1659,0
  FILEFLAGSMASK 0x3fL
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -414,13 +414,13 @@ BEGIN
             VALUE "Comments", "https://rufus.ie"
             VALUE "CompanyName", "Akeo Consulting"
             VALUE "FileDescription", "Rufus"
-            VALUE "FileVersion", "3.11.1658"
+            VALUE "FileVersion", "3.11.1659"
             VALUE "InternalName", "Rufus"
             VALUE "LegalCopyright", "© 2011-2020 Pete Batard (GPL v3)"
             VALUE "LegalTrademarks", "https://www.gnu.org/licenses/gpl-3.0.html"
             VALUE "OriginalFilename", "rufus-3.11.exe"
             VALUE "ProductName", "Rufus"
-            VALUE "ProductVersion", "3.11.1658"
+            VALUE "ProductVersion", "3.11.1659"
         END
     END
     BLOCK "VarFileInfo"

src/libcdio/cdio/rock.h
@@ -190,12 +190,12 @@ typedef struct iso_rock_nm_s {
 
 /*! Child link. See Section 4.1.5.1 */
 typedef struct iso_rock_cl_s {
-  char location[1];
+  iso733_t location;
 } GNUC_PACKED iso_rock_cl_t ;
 
 /*! Parent link. See Section 4.1.5.2 */
 typedef struct iso_rock_pl_s {
-  char location[1];
+  iso733_t location;
 } GNUC_PACKED iso_rock_pl_t ;
 
 /*! These are the bits and their meanings for flags in the TF structure. */
@@ -264,8 +264,28 @@ typedef struct iso_extension_record_s {
     iso_rock_cl_t  CL;  /**< Rock Ridge child link */
     iso_rock_pl_t  PL;  /**< Rock Ridge parent link */
     iso_rock_tf_t  TF;  /**< Rock Ridge timestamp(s) for a file */
+    iso_rock_sf_t  SF;  /**< Rock Ridge sparse file */
   } u;
 } GNUC_PACKED iso_extension_record_t;
+
+/* Bits for the u_su_fields of iso_rock_statbuf_t */
+#define ISO_ROCK_SUF_SP 0x00000001
+#define ISO_ROCK_SUF_ER 0x00000002
+#define ISO_ROCK_SUF_CE 0x00000004
+#define ISO_ROCK_SUF_PX 0x00000008
+#define ISO_ROCK_SUF_PN 0x00000010
+#define ISO_ROCK_SUF_SL 0x00000020
+#define ISO_ROCK_SUF_NM 0x00000040
+#define ISO_ROCK_SUF_TF 0x00000080
+#define ISO_ROCK_SUF_CL 0x00000100
+#define ISO_ROCK_SUF_PL 0x00000200
+#define ISO_ROCK_SUF_RE 0x00000400
+#define ISO_ROCK_SUF_SF 0x00000800
+
+#define ISO_ROCK_SUF_FORMAL (ISO_ROCK_SUF_ER | ISO_ROCK_SUF_PX | ISO_ROCK_SUF_PN | \
+                             ISO_ROCK_SUF_SL | ISO_ROCK_SUF_NM | ISO_ROCK_SUF_CL | \
+                             ISO_ROCK_SUF_PL | ISO_ROCK_SUF_RE | ISO_ROCK_SUF_TF | \
+                             ISO_ROCK_SUF_SF)
 
 typedef struct iso_rock_time_s {
   bool          b_used;     /**< If true, field has been set and  is valid. 
@@ -306,19 +326,23 @@ typedef struct iso_rock_statbuf_s {
                                          9660:9.5.6. */
   iso_rock_time_t effective;          /**< Effective time; See ISO 9660:9.5.7.
                                        */
-  uint32_t i_rdev;                    /**< the upper 16-bits is major device 
+  uint32_t        i_rdev;             /**< the upper 16-bits is major device 
                                          number, the lower 16-bits is the
                                          minor device number */
+  uint32_t        u_su_fields;        /**< System Use field attributes */
 
 } iso_rock_statbuf_t;
-  
+
 PRAGMA_END_PACKED
 
 /*! return length of name field; 0: not found, -1: to be ignored */
-int get_rock_ridge_filename(iso9660_dir_t * de, /*out*/ char * retname, 
+int get_rock_ridge_filename(iso9660_dir_t * de,
+                            /*in*/ void * p_iso,
+                            /*out*/ char * retname,
                             /*out*/ iso9660_stat_t *p_stat);
 
-  int parse_rock_ridge_stat(iso9660_dir_t *de, /*out*/ iso9660_stat_t *p_stat);
+int parse_rock_ridge_stat(iso9660_dir_t *de,
+                          /*out*/ iso9660_stat_t *p_stat);
 
   /*!
     Returns POSIX mode bitstring for a given file.

src/libcdio/driver/cdio_private.h
@@ -55,7 +55,7 @@ static inline char *libcdio_strndup(const char *s, size_t n)
     if (!result)
         return 0;
     result[len] = '\0';
-    return (char *) strncpy (result, s, len);
+    return (char *) memcpy (result, s, len);
 }
 #endif /*HAVE_STRNDUP*/
 
@@ -467,13 +467,23 @@ extern "C" {
 
   } cdio_funcs_t;
 
+  typedef struct {
+    uint16_t    u_type;
+    uint16_t    u_flags;
+  } cdio_header_t;
+
+#define CDIO_HEADER_TYPE_CDIO           0x0000
+#define CDIO_HEADER_TYPE_ISO            0x0001
+
+#define CDIO_HEADER_FLAGS_DISABLE_RR_DD 0x0001
 
   /*! Implementation of CdIo type */
   struct _CdIo {
-    driver_id_t driver_id; /**< Particular driver opened. */
-    cdio_funcs_t op;       /**< driver-specific routines handling
-                                implementation*/
-    void *env;             /**< environment. Passed to routine above. */
+    cdio_header_t header;    /**< Internal header - MUST come first. */
+    driver_id_t   driver_id; /**< Particular driver opened. */
+    cdio_funcs_t  op;        /**< driver-specific routines handling
+                                  implementation. */
+    void*         env;       /**< environment. Passed to routine above. */
   };
 
   /* This is used in drivers that must keep their own internal

src/libcdio/iso9660/iso9660_fs.c
@@ -61,6 +61,7 @@
 
 /** Implementation of iso9660_t type */
 struct _iso9660_s {
+  cdio_header_t header;     /**< Internal header - MUST come first. */
   CdioDataSource_t *stream; /**< Stream pointer */
   bool_3way_t b_xa;         /**< true if has XA attributes. */
   bool_3way_t b_mode2;      /**< true if has mode 2, false for mode 1. */
@@ -92,8 +93,7 @@ struct _iso9660_s {
 			         filesystem inside that it may be
 			         different.
 			     */
-    bool b_have_superblock; /**< Superblock has been read in? */
-
+  bool b_have_superblock;   /**< Superblock has been read in? */
 };
 
 static long int iso9660_seek_read_framesize (const iso9660_t *p_iso,
@@ -174,8 +174,10 @@ iso9660_open_ext_private (const char *psz_path,
 {
   iso9660_t *p_iso = (iso9660_t *) calloc(1, sizeof(iso9660_t)) ;
 
-  if (!p_iso) return NULL;
+  if (!p_iso)
+     return NULL;
 
+  p_iso->header.u_type = CDIO_HEADER_TYPE_ISO;
   p_iso->stream = cdio_stdio_new( psz_path );
   if (NULL == p_iso->stream)
     goto error;
@@ -778,19 +780,34 @@ iso9660_check_dir_block_end(iso9660_dir_t *p_iso9660_dir, unsigned *offset)
   return false;
 }
 
+static inline bool
+_iso9660_is_rock_ridge_enabled(void* p_image)
+{
+  cdio_header_t* p_header = (cdio_header_t*)p_image;
 
+  if (!p_header)
+    return false;
+  if (p_header->u_type == CDIO_HEADER_TYPE_ISO) {
+    iso9660_t* p_iso = (iso9660_t*)p_image;
+    return (p_iso->iso_extension_mask & ISO_EXTENSION_ROCK_RIDGE);
+  }
+  /* Rock Ridge is always enabled for other types */
+  return true;
+}
 
 static iso9660_stat_t *
 _iso9660_dir_to_statbuf (iso9660_dir_t *p_iso9660_dir,
 			 iso9660_stat_t *last_p_stat,
-			 bool_3way_t b_xa, uint8_t u_joliet_level)
+			 void* p_image,
+			 bool_3way_t b_xa,
+			 uint8_t u_joliet_level)
 {
   uint8_t dir_len= iso9660_get_dir_len(p_iso9660_dir);
   iso711_t i_fname;
   unsigned int stat_len;
   iso9660_stat_t *p_stat = last_p_stat;
   char rr_fname[256] = "";
-  int i_rr_fname;
+  int i_rr_fname = 0;
   lsn_t extent_lsn;
   bool first_extent;
 
@@ -814,6 +831,9 @@ _iso9660_dir_to_statbuf (iso9660_dir_t *p_iso9660_dir,
   }
   p_stat->type    = (p_iso9660_dir->file_flags & ISO_DIRECTORY)
     ? _STAT_DIR : _STAT_FILE;
+  /* Ignore Rock Ridge Deep Directory RE entries */
+  if (p_stat->rr.u_su_fields & ISO_ROCK_SUF_RE)
+    goto fail;
 
   /* Test for gaps between extents. Important: Use previous .total_size */
   extent_lsn = from_733 (p_iso9660_dir->extent);
@@ -831,8 +851,11 @@ _iso9660_dir_to_statbuf (iso9660_dir_t *p_iso9660_dir,
   /* Only now update .total_size */
   p_stat->total_size += from_733(p_iso9660_dir->size);
 
-  p_stat->rr.b3_rock = dunno; /*FIXME should do based on mask */
-  p_stat->b_xa    = false;
+#ifdef HAVE_ROCK
+  if (_iso9660_is_rock_ridge_enabled(p_image))
+    p_stat->rr.b3_rock = dunno;
+#endif
+  p_stat->b_xa = false;
 
 #ifndef DO_NOT_WANT_COMPATIBILITY
   if (first_extent) {
@@ -842,8 +865,7 @@ _iso9660_dir_to_statbuf (iso9660_dir_t *p_iso9660_dir,
 #endif /* DO_NOT_WANT_COMPATIBILITY */
 
   /* Only resolve the full filename when we're not dealing with extent */
-  if ((p_iso9660_dir->file_flags & ISO_MULTIEXTENT) == 0)
-  {
+  if ((p_iso9660_dir->file_flags & ISO_MULTIEXTENT) == 0) {
     /* Check if this is the last part of a multiextent file */
     if (!first_extent) {
       if (strlen(p_stat->filename) != i_fname ||
@@ -853,11 +875,9 @@ _iso9660_dir_to_statbuf (iso9660_dir_t *p_iso9660_dir,
 	goto fail;
       }
     }
-    i_rr_fname =
+
 #ifdef HAVE_ROCK
-      get_rock_ridge_filename(p_iso9660_dir, rr_fname, p_stat);
-#else
-      0;
+    i_rr_fname = get_rock_ridge_filename(p_iso9660_dir, p_image, rr_fname, p_stat);
 #endif
 
     if (i_rr_fname > 0) {
@@ -1024,7 +1044,7 @@ _fs_stat_root (CdIo_t *p_cdio)
     p_iso9660_dir = &(p_env->pvd.root_directory_record) ;
 #endif
 
-    p_stat = _iso9660_dir_to_statbuf (p_iso9660_dir, NULL,
+    p_stat = _iso9660_dir_to_statbuf (p_iso9660_dir, NULL, p_cdio,
 				      b_xa, p_env->u_joliet_level);
     return p_stat;
   }
@@ -1046,7 +1066,7 @@ _ifs_stat_root (iso9660_t *p_iso)
 #endif
 
   p_stat = _iso9660_dir_to_statbuf (p_iso9660_dir, NULL,
-				    p_iso->b_xa,
+				    p_iso, p_iso->b_xa,
 				    p_iso->u_joliet_level);
   return p_stat;
 }
@@ -1105,8 +1125,8 @@ _fs_stat_traverse (const CdIo_t *p_cdio, const iso9660_stat_t *_root,
 	/* Do not register remaining extents of ill file */
 	p_iso9660_stat = NULL;
       } else {
-	p_iso9660_stat = _iso9660_dir_to_statbuf (p_iso9660_dir,
-				p_iso9660_stat, dunno, p_env->u_joliet_level);
+	p_iso9660_stat = _iso9660_dir_to_statbuf (p_iso9660_dir, p_iso9660_stat,
+				(CdIo_t*)p_cdio, dunno, p_env->u_joliet_level);
 	if (NULL == p_iso9660_stat)
 	  skip_following_extents = true; /* Start ill file mode */
       }
@@ -1213,7 +1233,7 @@ _fs_iso_stat_traverse (iso9660_t *p_iso, const iso9660_stat_t *_root,
       if (iso9660_check_dir_block_end(p_iso9660_dir, &offset))
 	continue;
 
-      p_stat = _iso9660_dir_to_statbuf (p_iso9660_dir, p_stat,
+      p_stat = _iso9660_dir_to_statbuf (p_iso9660_dir, p_stat, p_iso,
 					p_iso->b_xa, p_iso->u_joliet_level);
 
       if (!p_stat) {
@@ -1476,8 +1496,8 @@ iso9660_fs_readdir (CdIo_t *p_cdio, const char psz_path[])
 	  p_iso9660_stat = NULL;
 	} else {
 	  p_iso9660_stat = _iso9660_dir_to_statbuf(p_iso9660_dir,
-						   p_iso9660_stat, dunno,
-						   p_env->u_joliet_level);
+						   p_iso9660_stat, p_cdio,
+						   dunno, p_env->u_joliet_level);
 	  if (NULL == p_iso9660_stat)
 	    skip_following_extents = true; /* Start ill file mode */
 	}
@@ -1570,10 +1590,13 @@ iso9660_ifs_readdir (iso9660_t *p_iso, const char psz_path[])
 	} else {
 	  p_iso9660_stat = _iso9660_dir_to_statbuf(p_iso9660_dir,
 						   p_iso9660_stat,
+						   p_iso,
 						   p_iso->b_xa,
 						   p_iso->u_joliet_level);
 	  if (NULL == p_iso9660_stat)
 	    skip_following_extents = true; /* Start ill file mode */
+	  else if (p_iso9660_stat->rr.u_su_fields & ISO_ROCK_SUF_RE)
+	    continue; /* Ignore RE entries */
 	}
 	if ((p_iso9660_dir->file_flags & ISO_MULTIEXTENT) == 0)
 	  skip_following_extents = false; /* Ill or not: The file ends now */
@@ -1752,6 +1775,55 @@ iso9660_ifs_find_lsn(iso9660_t *p_iso, lsn_t i_lsn)
   return ret;
 }
 
+#ifdef HAVE_ROCK
+/* Some compilers complain if the prototype is not defined */
+iso9660_stat_t *
+_iso9660_dd_find_lsn(void* p_image, lsn_t i_lsn);
+
+/* Same as above for Rock Ridge deep directory traversing. */
+iso9660_stat_t *
+_iso9660_dd_find_lsn(void* p_image, lsn_t i_lsn)
+{
+  cdio_header_t* p_header = (cdio_header_t*)p_image;
+  char* psz_full_filename = NULL;
+  void* p_image_dd;
+  iso9660_readdir_t* f_readdir;
+  iso9660_stat_t* ret;
+  size_t size;
+
+  switch(p_header->u_type) {
+  case CDIO_HEADER_TYPE_ISO:
+    size = sizeof(iso9660_t);
+    f_readdir = (iso9660_readdir_t*)iso9660_ifs_readdir;
+    break;
+  case CDIO_HEADER_TYPE_CDIO:
+    size = sizeof(CdIo_t);
+    f_readdir = (iso9660_readdir_t*)iso9660_fs_readdir;
+    break;
+  default:
+    cdio_assert(false);
+    return NULL;
+  }
+
+  /* Work with a duplicate to allow concurrency. */
+  p_image_dd = calloc(1, size);
+  if (!p_image_dd) {
+    cdio_warn("Memory duplication error");
+    return NULL;
+  }
+  memcpy(p_image_dd, p_image, size);
+
+  /* Disable the deep directory flag so we can process all entries */
+  p_header = (cdio_header_t*)p_image_dd;
+  p_header->u_flags |= CDIO_HEADER_FLAGS_DISABLE_RR_DD;
+  ret = find_lsn_recurse(p_image_dd, f_readdir, "/", i_lsn, &psz_full_filename);
+  if (psz_full_filename != NULL)
+    free(psz_full_filename);
+  free(p_image_dd);
+  return ret;
+}
+#endif /* HAVE ROCK */
+
 /*!
    Given a directory pointer, find the filesystem entry that contains
    lsn and return information about it.
@@ -1860,8 +1932,8 @@ iso_have_rr_traverse (iso9660_t *p_iso, const iso9660_stat_t *_root,
       if (iso9660_check_dir_block_end(p_iso9660_dir, &offset))
 	continue;
 
-      p_stat = _iso9660_dir_to_statbuf (p_iso9660_dir, NULL, p_iso->b_xa,
-					p_iso->u_joliet_level);
+      p_stat = _iso9660_dir_to_statbuf (p_iso9660_dir, NULL, p_iso,
+					p_iso->b_xa, p_iso->u_joliet_level);
       have_rr = p_stat->rr.b3_rock;
       if ( have_rr != yep) {
 	if (strlen(splitpath[0]) == 0)

commit a8f3c6c5721c5d7e9327361f11c301bf2cdc0719

[iso] fix detection of GRUB version
* Commit 77d319267f869d0d9eb9ac5a831164add8a5aa8d broke lookup of ISO filenames since iso9660_open()
  enabled the Rock Ridge extensions by default, despite using ISO_EXTENSION_NONE
  for the internal call, and we addressed a FIXME related to this.
* This resulted in Rufus not being able to lookup 'boot/grub/i386-pc/normal.mod' to parse GRUB's
  version, since without Rock Ridge, 'i386-pc/' is unable to match the ISO-9660 'I386_PC/' dir.
* Closes #1573 and addresses part of #1616.
* Also fix a MinGW compilation warning.

src/drive.c
@@ -406,7 +406,7 @@ char* AltGetLogicalName(DWORD DriveIndex, uint64_t PartitionOffset, BOOL bKeepTr
 	static_strcpy(volume_name, groot_name);
 	if (!QueryDosDeviceA(path, &volume_name[groot_len], (DWORD)(MAX_PATH - groot_len)) || (strlen(volume_name) < 20)) {
 		suprintf("Could not find a DOS volume name for '%s': %s", path, WindowsErrorString());
-			goto out;
+		goto out;
 	} else if (bKeepTrailingBackslash) {
 		static_strcat(volume_name, "\\");
 	}

src/iso.c
@@ -59,6 +59,10 @@
 #define S_IFLNK                   0xA000
 #define S_ISLNK(m)                (((m) & S_IFMT) == S_IFLNK)
 
+// Set the iso_open_ext() extension mask according to our global options
+#define ISO_EXTENSION_MASK        (ISO_EXTENSION_ALL & (enable_joliet ? ISO_EXTENSION_ALL : ~ISO_EXTENSION_JOLIET) & \
+                                  (enable_rockridge ? ISO_EXTENSION_ALL : ~ISO_EXTENSION_ROCK_RIDGE))
+
 // Needed for UDF ISO access
 CdIo_t* cdio_open (const char* psz_source, driver_id_t driver_id) {return NULL;}
 void cdio_destroy (CdIo_t* p_cdio) {}
@@ -1181,7 +1185,9 @@ int64_t ExtractISOFile(const char* iso, const char* iso_file, const char* dest_f
 	goto out;
 
 try_iso:
-	p_iso = iso9660_open(iso);
+	// Make sure to enable extensions, else we may not match the name of the file we are looking
+	// for since Rock Ridge may be needed to translate something like 'I386_PC' into 'i386-pc'...
+	p_iso = iso9660_open_ext(iso, ISO_EXTENSION_MASK);
 	if (p_iso == NULL) {
 		uprintf("Unable to open image '%s'", iso);
 		goto out;
@@ -1266,7 +1272,7 @@ uint32_t GetInstallWimVersion(const char* iso)
 	goto out;
 
 try_iso:
-	p_iso = iso9660_open(iso);
+	p_iso = iso9660_open_ext(iso, ISO_EXTENSION_MASK);
 	if (p_iso == NULL) {
 		uprintf("Could not open image '%s'", iso);
 		goto out;
@@ -1353,7 +1359,7 @@ BOOL HasEfiImgBootLoaders(void)
 	if ((image_path == NULL) || !HAS_EFI_IMG(img_report))
 		return FALSE;
 
-	p_iso = iso9660_open(image_path);
+	p_iso = iso9660_open_ext(image_path, ISO_EXTENSION_MASK);
 	if (p_iso == NULL) {
 		uprintf("Could not open image '%s' as an ISO-9660 file system", image_path);
 		goto out;
@@ -1444,7 +1450,7 @@ BOOL DumpFatDir(const char* path, int32_t cluster)
 		// Root dir => Perform init stuff
 		if (image_path == NULL)
 			return FALSE;
-		p_iso = iso9660_open(image_path);
+		p_iso = iso9660_open_ext(image_path, ISO_EXTENSION_MASK);
 		if (p_iso == NULL) {
 			uprintf("Could not open image '%s' as an ISO-9660 file system", image_path);
 			goto out;

src/rufus.h
@@ -482,7 +482,6 @@ extern char *image_path, *fido_url;
 /*
  * Shared prototypes
  */
-extern uint8_t popcnt8(uint8_t val);
 extern void GetWindowsVersion(void);
 extern BOOL is_x64(void);
 extern BOOL GetCpuArch(void);

src/rufus.rc
@@ -33,7 +33,7 @@ LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
 IDD_DIALOG DIALOGEX 12, 12, 232, 326
 STYLE DS_SETFONT | DS_MODALFRAME | DS_CENTER | WS_MINIMIZEBOX | WS_POPUP | WS_CAPTION | WS_SYSMENU
 EXSTYLE WS_EX_ACCEPTFILES
-CAPTION "Rufus 3.12.1701"
+CAPTION "Rufus 3.12.1702"
 FONT 9, "Segoe UI Symbol", 400, 0, 0x0
 BEGIN
     LTEXT           "Drive Properties",IDS_DRIVE_PROPERTIES_TXT,8,6,53,12,NOT WS_GROUP
@@ -395,8 +395,8 @@ END
 //
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 3,12,1701,0
- PRODUCTVERSION 3,12,1701,0
+ FILEVERSION 3,12,1702,0
+ PRODUCTVERSION 3,12,1702,0
  FILEFLAGSMASK 0x3fL
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -414,13 +414,13 @@ BEGIN
             VALUE "Comments", "https://rufus.ie"
             VALUE "CompanyName", "Akeo Consulting"
             VALUE "FileDescription", "Rufus"
-            VALUE "FileVersion", "3.12.1701"
+            VALUE "FileVersion", "3.12.1702"
             VALUE "InternalName", "Rufus"
             VALUE "LegalCopyright", "© 2011-2020 Pete Batard (GPL v3)"
             VALUE "LegalTrademarks", "https://www.gnu.org/licenses/gpl-3.0.html"
             VALUE "OriginalFilename", "rufus-3.12.exe"
             VALUE "ProductName", "Rufus"
-            VALUE "ProductVersion", "3.12.1701"
+            VALUE "ProductVersion", "3.12.1702"
         END
     END
     BLOCK "VarFileInfo"