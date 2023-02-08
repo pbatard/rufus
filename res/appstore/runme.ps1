3281f6b97effa7b8af77164b61e48b608cd64b87
Pete Batard <pete@akeo.ie>
2023-02-08 17:11:51 +0000

    [loc] set up Windows Store listing translations (part 2)
    * Add PowerShell script to generate listing.csv
    * Also update relevant messages along with French translation

--- res/appstore/runme.ps1
+++ res/appstore/runme.ps1
@@ -1,4 +1,19 @@
 # PowerShell script to parse listing.csv and retrieve our screenshots
+# Copyright © 2023 Pete Batard <pete@akeo.ie>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 try {
   [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 } catch {}
