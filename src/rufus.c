pbatard/rufus

commit 92a8926c18ba6ab56c01422123cbe886ce9f964f

[misc] clean up and refactor internal dialog handling
* Group the static elements in dialog-specific structs.
* Use 0 rather than -1 for unset elements.
* Also fix initial checkbox value for Silent Install not being properly set.

src/rufus.c
@@ -96,7 +96,6 @@ extern HIMAGELIST hUpImageList, hDownImageList;
 extern BOOL enable_iso, enable_joliet, enable_rockridge, enable_extra_hashes;
 extern BOOL validate_md5sum, cpu_has_sha1_accel, cpu_has_sha256_accel, toggle_dark_mode;
 extern BYTE* fido_script;
-extern HWND hFidoDlg;
 extern uint8_t* grub2_buf;
 extern long grub2_len;
 extern char* szStatusMessage;
@@ -122,7 +121,7 @@ WORD selected_langid = MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT);
 DWORD MainThreadId;
 USHORT NativeMachine = IMAGE_FILE_MACHINE_UNKNOWN;
 HWND hDeviceList, hPartitionScheme, hTargetSystem, hFileSystem, hClusterSize, hLabel, hBootType, hNBPasses, hLog = NULL;
-HWND hImageOption, hLogDialog = NULL, hProgress = NULL;
+HWND hImageOption, hLogDialog = NULL, hProgress = NULL, hFidoDlg = NULL;
 HANDLE dialog_handle = NULL, format_thread = NULL;
 BOOL is_x86_64, use_own_c32[NB_OLD_C32] = { FALSE, FALSE }, mbr_selected_by_user = FALSE, lock_drive = TRUE;
 BOOL op_in_progress = TRUE, right_to_left_mode = FALSE, has_uefi_csm = FALSE, its_a_me_mario = FALSE;
@@ -1503,7 +1502,7 @@ static DWORD WINAPI BootCheckThread(LPVOID param)
 	const char* syslinux = "syslinux";
 	const char* ldlinux_ext[3] = { "sys", "bss", "c32" };
 	char tmp[MAX_PATH], tmp2[MAX_PATH], c;
-	selection_dialog_options_t selection_options = { 0, -1, -1, -1, -1 };
+	selection_dialog_options_t selection = { 0 };
 
 	syslinux_ldlinux_len[0] = 0; syslinux_ldlinux_len[1] = 0;
 	safe_free(grub2_buf);
@@ -1550,19 +1549,25 @@ static DWORD WINAPI BootCheckThread(LPVOID param)
 				// but only do so if persistence has not been selected.
 				char* iso_image = lmprintf(MSG_036);
 				char* dd_image = lmprintf(MSG_095);
+				StrArrayCreate(&selection.choices, 4);
 				// If the ISO is small enough to be written as an ESP and we are using GPT add the ISO → ESP option
 				if ((img_report.projected_size < MAX_ISO_TO_ESP_SIZE) && HAS_REGULAR_EFI(img_report) &&
 					(partition_type == PARTITION_STYLE_GPT) && IS_FAT(fs_type)) {
-					char* choices[3] = { lmprintf(MSG_276, iso_image), lmprintf(MSG_277, "ISO → ESP"), lmprintf(MSG_277, dd_image) };
-					i = SelectionDialog(lmprintf(MSG_274, "ISOHybrid"), lmprintf(MSG_275, iso_image, dd_image, iso_image, dd_image), choices, 3);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_276, iso_image), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_277, "ISO → ESP"), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_277, dd_image), TRUE);
+					i = SelectionDialog(lmprintf(MSG_274, "ISOHybrid"), lmprintf(MSG_275, iso_image, dd_image, iso_image, dd_image), &selection);
+					StrArrayDestroy(&selection.choices);
 					if (i < 0)	// Cancel
 						goto out;
 					write_as_esp = (i & 2);
 					write_as_image = (i & 4);
 					esp_already_asked = TRUE;
 				} else {
-					char* choices[2] = { lmprintf(MSG_276, iso_image), lmprintf(MSG_277, dd_image) };
-					i = SelectionDialog(lmprintf(MSG_274, "ISOHybrid"), lmprintf(MSG_275, iso_image, dd_image, iso_image, dd_image), choices, 2);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_276, iso_image), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_277, dd_image), TRUE);
+					i = SelectionDialog(lmprintf(MSG_274, "ISOHybrid"), lmprintf(MSG_275, iso_image, dd_image, iso_image, dd_image), &selection);
+					StrArrayDestroy(&selection.choices);
 					if (i < 0)	// Cancel
 						goto out;
 					write_as_image = (i & 2);
@@ -1600,31 +1605,30 @@ static DWORD WINAPI BootCheckThread(LPVOID param)
 				break;
 			}
 			if ((WindowsVersion.Version >= WINDOWS_8) && IS_WINDOWS_1X(img_report)) {
-				StrArray options;
 				int arch = _log2(img_report.has_efi >> 1);
 				uint16_t map[16] = { 0 }, b = 1;
-				StrArrayCreate(&options, 8);
-				StrArrayAdd(&options, lmprintf(MSG_332), TRUE);
+				StrArrayCreate(&selection.choices, 8);
+				StrArrayAdd(&selection.choices, lmprintf(MSG_332), TRUE);
 				MAP_BIT(UNATTEND_OFFLINE_INTERNAL_DRIVES);
 				if (img_report.win_version.build >= 22500) {
-					StrArrayAdd(&options, lmprintf(MSG_330), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_330), TRUE);
 					MAP_BIT(UNATTEND_NO_ONLINE_ACCOUNT);
 				}
-				StrArrayAdd(&options, lmprintf(MSG_333), TRUE);
-				selection_options.username_index = _log2(b);
+				StrArrayAdd(&selection.choices, lmprintf(MSG_333), TRUE);
+				selection.username_index = _log2(b);
 				MAP_BIT(UNATTEND_SET_USER);
-				StrArrayAdd(&options, lmprintf(MSG_334), TRUE);
+				StrArrayAdd(&selection.choices, lmprintf(MSG_334), TRUE);
 				MAP_BIT(UNATTEND_DUPLICATE_LOCALE);
-				StrArrayAdd(&options, lmprintf(MSG_331), TRUE);
+				StrArrayAdd(&selection.choices, lmprintf(MSG_331), TRUE);
 				MAP_BIT(UNATTEND_NO_DATA_COLLECTION);
 				if (expert_mode) {
-					StrArrayAdd(&options, lmprintf(MSG_346), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_346), TRUE);
 					MAP_BIT(UNATTEND_FORCE_S_MODE);
 				}
-				selection_options.mask = remap16(unattend_xml_mask, map, FALSE);
-				i = CustomSelectionDialog(BS_AUTOCHECKBOX, lmprintf(MSG_327), lmprintf(MSG_328),
-					options.String, options.Index, &selection_options);
-				StrArrayDestroy(&options);
+				selection.mask = remap16(unattend_xml_mask, map, FALSE);
+				selection.style = BS_AUTOCHECKBOX;
+				i = SelectionDialog(lmprintf(MSG_327), lmprintf(MSG_328), &selection);
+				StrArrayDestroy(&selection.choices);
 				if (i < 0)
 					goto out;
 				// Remap i to the correct bit positions before calling CreateUnattendXml()
@@ -1665,50 +1669,49 @@ static DWORD WINAPI BootCheckThread(LPVOID param)
 				uprintf("NOTICE: A '/sources/$OEM$/$$/Panther/unattend.xml' was detected on the ISO.");
 				uprintf("As a result, the 'Windows User Experience dialog' will not be displayed.");
 			} else {
-				StrArray options;
 				int arch = _log2(img_report.has_efi >> 1);
 				uint16_t map[16] = { 0 }, b = 1;
-				StrArrayCreate(&options, 10);
+				StrArrayCreate(&selection.choices, 16);
 				if (IS_WINDOWS_11(img_report)) {
-					StrArrayAdd(&options, lmprintf(MSG_329), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_329), TRUE);
 					MAP_BIT(UNATTEND_SECUREBOOT_TPM_MINRAM);
 				}
 				if (img_report.win_version.build >= 22500) {
-					StrArrayAdd(&options, lmprintf(MSG_330), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_330), TRUE);
 					MAP_BIT(UNATTEND_NO_ONLINE_ACCOUNT);
 				}
-				StrArrayAdd(&options, lmprintf(MSG_333), TRUE);
-				selection_options.username_index = _log2(b);
+				StrArrayAdd(&selection.choices, lmprintf(MSG_333), TRUE);
+				selection.username_index = _log2(b);
 				MAP_BIT(UNATTEND_SET_USER);
-				StrArrayAdd(&options, lmprintf(MSG_334), TRUE);
-				selection_options.regional_index = _log2(b);
+				StrArrayAdd(&selection.choices, lmprintf(MSG_334), TRUE);
+				selection.regional_index = _log2(b);
 				MAP_BIT(UNATTEND_DUPLICATE_LOCALE);
-				StrArrayAdd(&options, lmprintf(MSG_331), TRUE);
-				selection_options.privacy_index = _log2(b);
+				StrArrayAdd(&selection.choices, lmprintf(MSG_331), TRUE);
+				selection.privacy_index = _log2(b);
 				MAP_BIT(UNATTEND_NO_DATA_COLLECTION);
 				if (IS_WINDOWS_11(img_report)) {
-					StrArrayAdd(&options, lmprintf(MSG_335), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_335), TRUE);
 					MAP_BIT(UNATTEND_DISABLE_BITLOCKER);
-					StrArrayAdd(&options, lmprintf(MSG_324), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_324), TRUE);
 					MAP_BIT(UNATTEND_QOL_ENHANCEMENTS);
-					StrArrayAdd(&options, lmprintf(MSG_355), TRUE);
-					selection_options.edition_index = _log2(b);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_355), TRUE);
+					selection.edition_index = _log2(b);
 					MAP_BIT(UNATTEND_SILENT_INSTALL);
 					if (img_report.win_version.build >= 26200) {
-						StrArrayAdd(&options, lmprintf(MSG_350), TRUE);
+						StrArrayAdd(&selection.choices, lmprintf(MSG_350), TRUE);
 						MAP_BIT(UNATTEND_USE_MS2023_BOOTLOADERS);
-						StrArrayAdd(&options, lmprintf(MSG_323), TRUE);
+						StrArrayAdd(&selection.choices, lmprintf(MSG_323), TRUE);
 						MAP_BIT(UNATTEND_APPLY_SKUSIPOLICY);
 					}
 				}
 				if (expert_mode) {
-					StrArrayAdd(&options, lmprintf(MSG_346), TRUE);
+					StrArrayAdd(&selection.choices, lmprintf(MSG_346), TRUE);
 					MAP_BIT(UNATTEND_FORCE_S_MODE);
 				}
-				selection_options.mask = remap16(unattend_xml_mask, map, FALSE);
-				i = CustomSelectionDialog(BS_AUTOCHECKBOX, lmprintf(MSG_327), lmprintf(MSG_328),
-					options.String, options.Index, &selection_options);
-				StrArrayDestroy(&options);
+				selection.mask = remap16(unattend_xml_mask, map, FALSE);
+				selection.style = BS_AUTOCHECKBOX;
+				i = SelectionDialog(lmprintf(MSG_327), lmprintf(MSG_328), &selection);
+				StrArrayDestroy(&selection.choices);
 				if (i < 0)
 					goto out;
 				i = remap16(i, map, TRUE);
@@ -1725,8 +1728,11 @@ static DWORD WINAPI BootCheckThread(LPVOID param)
 			// The ISO is small enough to be written as an ESP and we are using GPT
 			// so ask the users if they want to write it as an ESP.
 			char* iso_image = lmprintf(MSG_036);
-			char* choices[2] = { lmprintf(MSG_276, iso_image), lmprintf(MSG_277, "ISO → ESP") };
-			i = SelectionDialog(lmprintf(MSG_274, "ESP"), lmprintf(MSG_310), choices, 2);
+			StrArrayCreate(&selection.choices, 2);
+			StrArrayAdd(&selection.choices, lmprintf(MSG_276, iso_image), TRUE);
+			StrArrayAdd(&selection.choices, lmprintf(MSG_277, "ISO → ESP"), TRUE);
+			i = SelectionDialog(lmprintf(MSG_274, "ESP"), lmprintf(MSG_310), &selection);
+			StrArrayDestroy(&selection.choices);
 			if (i < 0)	// Cancel
 				goto out;
 			write_as_esp = (i & 2);

src/rufus.rc
@@ -33,7 +33,7 @@ LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
 IDD_DIALOG DIALOGEX 12, 12, 232, 326
 STYLE DS_SETFONT | DS_MODALFRAME | DS_CENTER | WS_MINIMIZEBOX | WS_POPUP | WS_CAPTION | WS_SYSMENU
 EXSTYLE WS_EX_ACCEPTFILES
-CAPTION "Rufus 4.14.2321"
+CAPTION "Rufus 4.14.2322"
 FONT 9, "Segoe UI Symbol", 400, 0, 0x0
 BEGIN
     LTEXT           "Drive Properties",IDS_DRIVE_PROPERTIES_TXT,8,6,53,12,NOT WS_GROUP
@@ -409,8 +409,8 @@ END
 //
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 4,14,2321,0
- PRODUCTVERSION 4,14,2321,0
+ FILEVERSION 4,14,2322,0
+ PRODUCTVERSION 4,14,2322,0
  FILEFLAGSMASK 0x3fL
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -428,13 +428,13 @@ BEGIN
             VALUE "Comments", "https://rufus.ie"
             VALUE "CompanyName", "Akeo Consulting"
             VALUE "FileDescription", "Rufus"
-            VALUE "FileVersion", "4.14.2321"
+            VALUE "FileVersion", "4.14.2322"
             VALUE "InternalName", "Rufus"
             VALUE "LegalCopyright", "© 2011-2026 Pete Batard (GPL v3)"
             VALUE "LegalTrademarks", "https://www.gnu.org/licenses/gpl-3.0.html"
             VALUE "OriginalFilename", "rufus-4.14.exe"
             VALUE "ProductName", "Rufus"
-            VALUE "ProductVersion", "4.14.2321"
+            VALUE "ProductVersion", "4.14.2322"
         END
     END
     BLOCK "VarFileInfo"

src/rufus.h
@@ -507,15 +507,6 @@ typedef struct {
 	char* Label;
 } IMG_SAVE;
 
-// Options for the custom selection dialog
-typedef struct {
-	int mask;
-	int username_index;
-	int edition_index;
-	int regional_index;
-	int privacy_index;
-} selection_dialog_options_t;
-
 /*
  * Structure and macros used for the extensions specification of FileDialog()
  * You can use:
@@ -745,6 +736,17 @@ extern void StrArrayClear(StrArray* arr);
 extern void StrArrayDestroy(StrArray* arr);
 #define IsStrArrayEmpty(arr) (arr.Index == 0)
 
+// Options for the custom selection dialog
+typedef struct {
+	int style;
+	int mask;
+	int username_index;
+	int edition_index;
+	int regional_index;
+	int privacy_index;
+	StrArray choices;
+} selection_dialog_options_t;
+
 /*
  * Globals
  */
@@ -819,8 +821,7 @@ extern void DestroyTooltip(HWND hWnd);
 extern void DestroyAllTooltips(void);
 extern int NotificationEx(int type, const char* dont_display_setting, const notification_info* more_info, const char* title, const char* format, ...);
 #define Notification(type, title, ...) NotificationEx(type, NULL, NULL, title, __VA_ARGS__)
-extern int CustomSelectionDialog(int style, char* title, char* message, char** choices, int size, selection_dialog_options_t* options);
-#define SelectionDialog(title, message, choices, size) CustomSelectionDialog(BS_AUTORADIOBUTTON, title, message, choices, size, NULL)
+extern int SelectionDialog(char* title, char* message, selection_dialog_options_t* options);
 extern void ListDialog(char* title, char* message, char** items, int size);
 extern SIZE GetTextSize(HWND hCtrl, char* txt);
 extern BOOL ExtractAppIcon(const char* filename, BOOL bSilent);

src/wue.c
@@ -734,8 +734,9 @@ int GetEditions(StrArray* version_name, StrArray* version_index)
 {
 	int i, r, n = -1;
 	WIMStruct* wim = NULL;
+	selection_dialog_options_t selection = { 0 };
 	const char* edition_suffix;
-	char *install_names[MAX_WININST], *edition_name;
+	char *edition_name;
 	wchar_t wim_path[4 * MAX_PATH] = L"", * xml = NULL;
 	size_t xml_len;
 	BOOL bNonStandard = FALSE;
@@ -743,9 +744,11 @@ int GetEditions(StrArray* version_name, StrArray* version_index)
 
 	// If we have multiple windows install images, ask the user the one to use
 	if (img_report.wininst_index > 1) {
+		StrArrayCreate(&selection.choices, 16);
 		for (i = 0; i < img_report.wininst_index; i++)
-			install_names[i] = &img_report.wininst_path[i][2];
-		wininst_index = _log2(SelectionDialog(lmprintf(MSG_130), lmprintf(MSG_131), install_names, img_report.wininst_index));
+			StrArrayAdd(&selection.choices, &img_report.wininst_path[i][2], TRUE);
+		wininst_index = _log2(SelectionDialog(lmprintf(MSG_130), lmprintf(MSG_131), &selection));
+		StrArrayDestroy(&selection.choices);
 		if (wininst_index < 0)
 			return -2;
 		if (wininst_index >= MAX_WININST)
@@ -820,12 +823,12 @@ int SetWinToGoIndex(void)
 {
 	int i, r;
 	WIMStruct* wim = NULL;
-	char* install_names[MAX_WININST];
 	wchar_t wim_path[4 * MAX_PATH] = L"", *xml = NULL;
 	size_t xml_len;
-	StrArray version_name = { 0 }, version_index = { 0 };
+	StrArray version_index = { 0 };
 	BOOL bNonStandard = FALSE;
 	ezxml_t index = NULL, image = NULL;
+	selection_dialog_options_t selection = { 0 };
 
 	// Sanity checks
 	wintogo_index = -1;
@@ -835,9 +838,11 @@ int SetWinToGoIndex(void)
 
 	// If we have multiple windows install images, ask the user the one to use
 	if (img_report.wininst_index > 1) {
+		StrArrayCreate(&selection.choices, 16);
 		for (i = 0; i < img_report.wininst_index; i++)
-			install_names[i] = &img_report.wininst_path[i][2];
-		wininst_index = _log2(SelectionDialog(lmprintf(MSG_130), lmprintf(MSG_131), install_names, img_report.wininst_index));
+			StrArrayAdd(&selection.choices, &img_report.wininst_path[i][2], TRUE);
+		wininst_index = _log2(SelectionDialog(lmprintf(MSG_130), lmprintf(MSG_131), &selection));
+		StrArrayDestroy(&selection.choices);
 		if (wininst_index < 0)
 			return -2;
 		if (wininst_index >= MAX_WININST)
@@ -864,7 +869,7 @@ int SetWinToGoIndex(void)
 		goto out;
 	}
 
-	StrArrayCreate(&version_name, 16);
+	StrArrayCreate(&selection.choices, 16);
 	StrArrayCreate(&version_index, 16);
 	index = ezxml_parse_str((char*)xml, xml_len);
 	if (index == NULL) {
@@ -877,10 +882,10 @@ int SetWinToGoIndex(void)
 		image = image->next, i++) {
 		// Some people are apparently creating *unofficial* Windows ISOs that don't have DISPLAYNAME elements.
 		// If we are parsing such an ISO, try to fall back to using DESCRIPTION.
-		if (StrArrayAdd(&version_name, ezxml_child_val(image, "DISPLAYNAME"), TRUE) < 0) {
-			if (StrArrayAdd(&version_name, ezxml_child_val(image, "DESCRIPTION"), TRUE) < 0) {
+		if (StrArrayAdd(&selection.choices, ezxml_child_val(image, "DISPLAYNAME"), TRUE) < 0) {
+			if (StrArrayAdd(&selection.choices, ezxml_child_val(image, "DESCRIPTION"), TRUE) < 0) {
 				uprintf("WARNING: Could not find a description for image index %d", i + 1);
-				StrArrayAdd(&version_name, "Unknown Windows Version", TRUE);
+				StrArrayAdd(&selection.choices, "Unknown Windows Version", TRUE);
 			}
 			bNonStandard = TRUE;
 		}
@@ -890,7 +895,7 @@ int SetWinToGoIndex(void)
 
 	if (i > 1)
 		// NB: _log2 returns -2 if SelectionDialog() returns negative (user cancelled)
-		i = _log2(SelectionDialog(lmprintf(MSG_291), lmprintf(MSG_292), version_name.String, i)) + 1;
+		i = _log2(SelectionDialog(lmprintf(MSG_291), lmprintf(MSG_292), &selection)) + 1;
 	if (i < 0)
 		wintogo_index = -2;	// Cancelled by the user
 	else if (i == 0)
@@ -904,7 +909,7 @@ int SetWinToGoIndex(void)
 		if (img_report.win_version.major == 0 || img_report.win_version.build == 0)
 			uprintf("WARNING: Could not obtain version information from XML index (Nonstandard Windows image?)");
 		uprintf("Will use '%s' (Build: %d, Index %s) for Windows To Go",
-			version_name.String[i - 1], img_report.win_version.build, version_index.String[i - 1]);
+			selection.choices.String[i - 1], img_report.win_version.build, version_index.String[i - 1]);
 		// Need Windows 10 Creator Update or later for boot on REMOVABLE to work
 		if ((img_report.win_version.build < 15000) && (SelectedDrive.MediaType != FixedMedia)) {
 			if (Notification(MB_YESNO | MB_ICONWARNING, lmprintf(MSG_190), lmprintf(MSG_098)) != IDYES)
@@ -920,7 +925,7 @@ int SetWinToGoIndex(void)
 	}
 
 out:
-	StrArrayDestroy(&version_name);
+	StrArrayDestroy(&selection.choices);
 	StrArrayDestroy(&version_index);
 	free(xml);
 	ezxml_free(index);

commit 984c952d540cd9411dd716fa211ffe7cb1a23e26

[wue] fix username field not showing for Windows 10 Windows To Go
* This fixes a regression introduced in 92a8926c18ba6ab56c01422123cbe886ce9f964f with the index elements.
* Also update microsoft/setup-msbuild Action to v3 (Closes #2942).

.github/workflows/codeql.yml
@@ -49,7 +49,7 @@ jobs:
         languages: cpp
 
     - name: Add MSBuild to PATH
-      uses: microsoft/setup-msbuild@v2
+      uses: microsoft/setup-msbuild@v3
       with:
         msbuild-architecture: x64
 

.github/workflows/coverity.yml
@@ -45,7 +45,7 @@ jobs:
       run: echo "${{github.workspace}}/cov-analysis-win64/bin" >> $GITHUB_PATH
 
     - name: Add MSBuild to PATH
-      uses: microsoft/setup-msbuild@v2
+      uses: microsoft/setup-msbuild@v3
 
     - name: Build with Coverity
       run: | 

.github/workflows/setup.yml
@@ -22,7 +22,7 @@ jobs:
       uses: actions/checkout@v6
 
     - name: Add MSBuild to PATH
-      uses: microsoft/setup-msbuild@v2
+      uses: microsoft/setup-msbuild@v3
       with:
         msbuild-architecture: x64
 

.github/workflows/vs2022.yml
@@ -46,7 +46,7 @@ jobs:
         submodules: recursive
 
     - name: Add MSBuild to PATH
-      uses: microsoft/setup-msbuild@v2
+      uses: microsoft/setup-msbuild@v3
       with:
         msbuild-architecture: x64
 

src/rufus.c
@@ -1624,7 +1624,7 @@ static DWORD WINAPI BootCheckThread(LPVOID param)
 				}
 				StrArrayAdd(&selection.choices, lmprintf(MSG_333), TRUE);
 				StrArrayAdd(&selection.tooltips, lmprintf(MSG_364), TRUE);
-				selection.username_index = _log2(b);
+				selection.username_index = _log2(b) + 1;
 				MAP_BIT(UNATTEND_SET_USER);
 				StrArrayAdd(&selection.choices, lmprintf(MSG_334), TRUE);
 				StrArrayAdd(&selection.tooltips, lmprintf(MSG_365), TRUE);
@@ -1708,15 +1708,15 @@ static DWORD WINAPI BootCheckThread(LPVOID param)
 				}
 				StrArrayAdd(&selection.choices, lmprintf(MSG_333), TRUE);
 				StrArrayAdd(&selection.tooltips, lmprintf(MSG_364), TRUE);
-				selection.username_index = _log2(b);
+				selection.username_index = _log2(b) + 1;
 				MAP_BIT(UNATTEND_SET_USER);
 				StrArrayAdd(&selection.choices, lmprintf(MSG_334), TRUE);
 				StrArrayAdd(&selection.tooltips, lmprintf(MSG_365), TRUE);
-				selection.regional_index = _log2(b);
+				selection.regional_index = _log2(b) + 1;
 				MAP_BIT(UNATTEND_DUPLICATE_LOCALE);
 				StrArrayAdd(&selection.choices, lmprintf(MSG_331), TRUE);
 				StrArrayAdd(&selection.tooltips, lmprintf(MSG_362), TRUE);
-				selection.privacy_index = _log2(b);
+				selection.privacy_index = _log2(b) + 1;
 				MAP_BIT(UNATTEND_NO_DATA_COLLECTION);
 				if (IS_WINDOWS_11(img_report)) {
 					StrArrayAdd(&selection.choices, lmprintf(MSG_335), TRUE);
@@ -1727,7 +1727,7 @@ static DWORD WINAPI BootCheckThread(LPVOID param)
 					MAP_BIT(UNATTEND_QOL_ENHANCEMENTS);
 					StrArrayAdd(&selection.choices, lmprintf(MSG_355), TRUE);
 					StrArrayAdd(&selection.tooltips, lmprintf(MSG_371), TRUE);
-					selection.edition_index = _log2(b);
+					selection.edition_index = _log2(b) + 1;
 					MAP_BIT(UNATTEND_SILENT_INSTALL);
 					if (img_report.win_version.build >= 26200) {
 						StrArrayAdd(&selection.choices, lmprintf(MSG_350), TRUE);

src/rufus.rc
@@ -33,7 +33,7 @@ LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
 IDD_DIALOG DIALOGEX 12, 12, 232, 326
 STYLE DS_SETFONT | DS_MODALFRAME | DS_CENTER | WS_MINIMIZEBOX | WS_POPUP | WS_CAPTION | WS_SYSMENU
 EXSTYLE WS_EX_ACCEPTFILES
-CAPTION "Rufus 4.14.2337"
+CAPTION "Rufus 4.14.2338"
 FONT 9, "Segoe UI Symbol", 400, 0, 0x0
 BEGIN
     LTEXT           "Drive Properties",IDS_DRIVE_PROPERTIES_TXT,8,6,53,12,NOT WS_GROUP
@@ -409,8 +409,8 @@ END
 //
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 4,14,2337,0
- PRODUCTVERSION 4,14,2337,0
+ FILEVERSION 4,14,2338,0
+ PRODUCTVERSION 4,14,2338,0
  FILEFLAGSMASK 0x3fL
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -428,13 +428,13 @@ BEGIN
             VALUE "Comments", "https://rufus.ie"
             VALUE "CompanyName", "Akeo Consulting"
             VALUE "FileDescription", "Rufus"
-            VALUE "FileVersion", "4.14.2337"
+            VALUE "FileVersion", "4.14.2338"
             VALUE "InternalName", "Rufus"
             VALUE "LegalCopyright", "© 2011-2026 Pete Batard (GPL v3)"
             VALUE "LegalTrademarks", "https://www.gnu.org/licenses/gpl-3.0.html"
             VALUE "OriginalFilename", "rufus-4.14.exe"
             VALUE "ProductName", "Rufus"
-            VALUE "ProductVersion", "4.14.2337"
+            VALUE "ProductVersion", "4.14.2338"
         END
     END
     BLOCK "VarFileInfo"

src/stdlg.c
@@ -917,10 +917,10 @@ static INT_PTR CALLBACK SelectionCallback(HWND hDlg, UINT message, WPARAM wParam
 			SetWindowTextU(hCtrl, str);
 			ShowWindow(hCtrl, SW_SHOW);
 			// Compute the maximum line's width (with some extra for the username and edition fields)
-			if (i == selection_data.options->username_index) {
+			if (i == selection_data.options->username_index - 1) {
 				static_sprintf(str, "%s __%s__", selection_data.options->choices.String[i], base_username);
 				mw = max(mw, GetTextSize(hCtrl, str).cx);
-			} else if (i == selection_data.options->edition_index) {
+			} else if (i == selection_data.options->edition_index - 1) {
 				mw = max(mw, GetTextSize(hCtrl, str).cx +
 					GetComboBoxMinWidth(GetDlgItem(hDlg, IDC_SELECTION_EDITION), &edition_name));
 			} else {
@@ -949,10 +949,10 @@ static INT_PTR CALLBACK SelectionCallback(HWND hDlg, UINT message, WPARAM wParam
 		// If required, set up the the username edit box
 		if (selection_data.options->username_index > 0) {
 			unattend_username[0] = 0;
-			hCtrl = GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->username_index);
+			hCtrl = GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->username_index - 1);
 			GetClientRect(hCtrl, &rc);
 			ResizeMoveCtrl(hDlg, hCtrl, 0, 0,
-				(rc.left - rc.right) + GetTextSize(hCtrl, selection_data.options->choices.String[selection_data.options->username_index]).cx + ddw, 0, 1.0f);
+				(rc.left - rc.right) + GetTextSize(hCtrl, selection_data.options->choices.String[selection_data.options->username_index - 1]).cx + ddw, 0, 1.0f);
 			GetWindowRect(hCtrl, &rc);
 			SetWindowPos(GetDlgItem(hDlg, IDC_SELECTION_USERNAME), hCtrl, rc.left, rc.top, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 			hCtrl = GetDlgItem(hDlg, IDC_SELECTION_USERNAME);
@@ -967,10 +967,10 @@ static INT_PTR CALLBACK SelectionCallback(HWND hDlg, UINT message, WPARAM wParam
 
 		// If required, set up the the edition combo box
 		if (selection_data.options->edition_index > 0 && edition_name.String != NULL && edition_name.Index > 0) {
-			hCtrl = GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->edition_index);
+			hCtrl = GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->edition_index - 1);
 			GetClientRect(hCtrl, &rc);
 			ResizeMoveCtrl(hDlg, hCtrl, 0, 0,
-				(rc.left - rc.right) + GetTextSize(hCtrl, selection_data.options->choices.String[selection_data.options->edition_index]).cx + ddw, 0, 1.0f);
+				(rc.left - rc.right) + GetTextSize(hCtrl, selection_data.options->choices.String[selection_data.options->edition_index - 1]).cx + ddw, 0, 1.0f);
 			GetWindowRect(hCtrl, &rc);
 			SetWindowPos(GetDlgItem(hDlg, IDC_SELECTION_EDITION), hCtrl, rc.left, rc.top, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 			hCtrl = GetDlgItem(hDlg, IDC_SELECTION_EDITION);
@@ -1008,10 +1008,10 @@ static INT_PTR CALLBACK SelectionCallback(HWND hDlg, UINT message, WPARAM wParam
 		if (selection_data.options->edition_index > 0 && selection_data.options->username_index > 0 &&
 			selection_data.options->regional_index > 0 && selection_data.options->privacy_index > 0) {
 			// Should be the same condition as the one in WM_COMMAND
-			BOOL enable = Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->username_index)) &&
-				Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->regional_index)) &&
-				Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->privacy_index));
-			EnableWindow(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->edition_index), enable);
+			BOOL enable = Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->username_index - 1)) &&
+				Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->regional_index - 1)) &&
+				Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->privacy_index - 1));
+			EnableWindow(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->edition_index - 1), enable);
 			EnableWindow(GetDlgItem(hDlg, IDC_SELECTION_EDITION), enable);
 		}
 
@@ -1044,13 +1044,13 @@ static INT_PTR CALLBACK SelectionCallback(HWND hDlg, UINT message, WPARAM wParam
 			// Check if local account + regional settings + data collection checkboxes are clicked and enable/disable the silent install option
 			if (selection_data.options->edition_index > 0 && selection_data.options->username_index > 0 &&
 				selection_data.options->edition_index > 0 && selection_data.options->regional_index > 0 &&
-					(command - IDC_SELECTION_CHOICE1 == selection_data.options->username_index ||
-					 command - IDC_SELECTION_CHOICE1 == selection_data.options->regional_index ||
-					 command - IDC_SELECTION_CHOICE1 == selection_data.options->privacy_index)) {
-				BOOL enable = Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->username_index)) &&
-					Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->regional_index)) &&
-					Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->privacy_index));
-				hCtrl = GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->edition_index);
+					(command - IDC_SELECTION_CHOICE1 == selection_data.options->username_index - 1 ||
+					 command - IDC_SELECTION_CHOICE1 == selection_data.options->regional_index - 1 ||
+					 command - IDC_SELECTION_CHOICE1 == selection_data.options->privacy_index - 1)) {
+				BOOL enable = Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->username_index - 1)) &&
+					Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->regional_index - 1)) &&
+					Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->privacy_index - 1));
+				hCtrl = GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->edition_index - 1);
 				if (!enable && IsWindowEnabled(hCtrl)) {
 					silent_install_checked = (Button_GetCheck(hCtrl) == BST_CHECKED);
 					Button_SetCheck(hCtrl, FALSE);
@@ -1064,7 +1064,7 @@ static INT_PTR CALLBACK SelectionCallback(HWND hDlg, UINT message, WPARAM wParam
 		case IDOK:
 			// Produce a big scary warning if the silent install option was selected
 			if (selection_data.options->edition_index > 0 &&
-				Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->edition_index)) &&
+				Button_GetCheck(GetDlgItem(hDlg, IDC_SELECTION_CHOICE1 + selection_data.options->edition_index - 1)) &&
 				Notification(MB_YESNO | MB_ICONWARNING, APPLICATION_NAME, lmprintf(MSG_356)) != IDYES)
 				break;
 			for (r = 0, i = 0, m = 1; i < nDialogItems; i++, m <<= 1)